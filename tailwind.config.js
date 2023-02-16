709516d604a895a868cdf576cf03724b8b943a35
Tom <tom.bost.pro@gmail.com>
2023-02-16 17:15:34 +0100

    feat(tailwindcss): add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			poppins: ["Poppins", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				myDarkblue: "#0C151D",
+				myDarklight: "#171F26",
+				myRed: "#95190E",
+				myWhite: "#E5E5E5",
+				darkText: "#A3ABB2",
+				lightText: "#3D3D3D",
+			},
+		},
 	},
 	plugins: [],
+	darkMode: "class",
 };
