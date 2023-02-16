8469a919a219edd1176b053bf47f4dc6c8156786
Tom <tom.bost.pro@gmail.com>
2023-02-16 18:08:49 +0100

    fix(profile pic): fix for safari

--- src/components/ProfilePic/ProfilePic.jsx
+++ src/components/ProfilePic/ProfilePic.jsx
@@ -4,8 +4,8 @@ import profilePic from "../../../public/image/1675265277430.jpeg";
 const ProfilePic = () => {
 	return (
 		<div className="col-span-2 col-start-2">
-			<figure className="overflow-hidden rounded-full outline-myRed outline-4 outline outline-offset-4">
-				<Image src={profilePic} className={""} />
+			<figure className="overflow-hidden rounded-full ring-myRed ring-4 ring-offset-4 ring-offset-myDarkblue">
+				<Image src={profilePic} />
 			</figure>
 		</div>
 	);
