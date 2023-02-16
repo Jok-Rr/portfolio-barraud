5c5fa980f419a25df00312ed8081708bee6d3e43
Tom <tom.bost.pro@gmail.com>
2023-02-16 18:20:46 +0100

    update(picprofile): add desc

--- src/components/ProfilePic/ProfilePic.jsx
+++ src/components/ProfilePic/ProfilePic.jsx
@@ -3,11 +3,20 @@ import profilePic from "../../../public/image/1675265277430.jpeg";
 
 const ProfilePic = () => {
 	return (
-		<div className="col-span-2 col-start-2">
-			<figure className="overflow-hidden rounded-full ring-myRed ring-[6px] ring-offset-[6px] ring-offset-myWhite dark:ring-offset-myDarkblue">
-				<Image src={profilePic} />
-			</figure>
-		</div>
+		<>
+			<div className="w-1/2 col-span-4 m-auto">
+				<figure className="overflow-hidden rounded-full ring-myRed ring-[6px] ring-offset-[6px] ring-offset-myWhite dark:ring-offset-myDarkblue ">
+					<Image src={profilePic} />
+				</figure>
+			</div>
+			<div className="col-span-4 text-center text-">
+				<h1>Florian Barraud</h1>
+				<p className="mb-2">Graphiste / Marketing</p>
+				<div className="flex justify-center">
+					<i class="fa-brands fa-twitter text-3xl"></i>
+				</div>
+			</div>
+		</>
 	);
 };
 
