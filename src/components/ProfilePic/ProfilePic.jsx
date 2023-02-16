8ab41b2e90b292454a08abd4bab9a50fd6fe2d42
Tom <tom.bost.pro@gmail.com>
2023-02-16 18:11:33 +0100

    update(profilpic): change color

--- src/components/ProfilePic/ProfilePic.jsx
+++ src/components/ProfilePic/ProfilePic.jsx
@@ -4,7 +4,7 @@ import profilePic from "../../../public/image/1675265277430.jpeg";
 const ProfilePic = () => {
 	return (
 		<div className="col-span-2 col-start-2">
-			<figure className="overflow-hidden rounded-full ring-myRed ring-4 ring-offset-4 ring-offset-myDarkblue">
+			<figure className="overflow-hidden rounded-full ring-myRed ring-[6px] ring-offset-[6px] ring-offset-myWhite dark:ring-offset-myDarkblue">
 				<Image src={profilePic} />
 			</figure>
 		</div>
