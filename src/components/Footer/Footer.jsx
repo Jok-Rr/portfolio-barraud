0d00a4d06a9f84a9af313f4dac7d7b1bde6863cf
Tom <tom.bost.pro@gmail.com>
2023-02-17 14:07:42 +0100

    feat(footer): Footer layout

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <div className="col-span-4 text-center">
+            <p>
+                © Florian Barraud. 2023 All rigths reserved - Développé par{' '}
+                <a href="https://tombost.fr" className="font-black text-myRed" target={'_blank'}>
+                    Tom Bost
+                </a>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
